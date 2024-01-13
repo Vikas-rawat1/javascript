84026419cb821ace82f975f8c2b01706039fbabd
vikas <121391039+Chill1bro@users.noreply.github.com>
2024-01-13 17:29:40 +0000

    container config

--- objects/object2.js
+++ objects/object2.js
@@ -21,10 +21,28 @@
 
 // console.log(regularUser.email)
 
-const obj1={1:"a",2:"b"}
-const obj2={3:"c",4:"d"}
+// const obj1={1:"a",2:"b"}
+// const obj2={3:"c",4:"d"}
 
-// const obj3={obj1 , obj2}
-const obj3 = Object.assign(obj1,obj2)
-// const obj3 = Object.assign({},obj1,obj2)
-console.log(obj3);
+
+// // const obj3={obj1 , obj2}
+// const obj3 = Object.assign(obj1,obj2)
+// // const obj3 = Object.assign({},obj1,obj2)
+// console.log(obj3);
+
+
+// OBJECT DESTRUCTURE 
+const course={
+    coursename:"js",
+    price:'999'
+}
+// console.log(course)
+
+// course.coursename
+
+// const {coursename}=course
+
+// THIS WILL GIVE US A SAME VALUE :--object destructuring                                     
+const {coursename:changecourse}=course
+
+console.log(changecourse)
